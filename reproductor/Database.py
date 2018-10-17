a9d46f9bf79160084c0cc3f81a6fe1a832220dd6
Laboratorio de Cómputo para la Investigación <c4rlab@correo.nucleares.unam.mx>
2018-10-17 17:55:25 -0500

    implementadas las búsquedas

--- reproductor/Database.py
+++ reproductor/Database.py
@@ -7,12 +7,11 @@ class Database:
     def __init__(self):
         pass
 
-    def insert_in_rola(self,data, dataAlbum):
+    def insert_in_rola(self,data):
         conn = sqlite3.connect('./../../../../../root/rolas.db')
         db = conn.cursor()
         try:
-                db.execute("insert into rolas ( path, title, track, year) values (?,?,?,?)",data)
-                db.execute("insert into albums(path, name, year) values (?,?,?)",dataAlbum)
+                db.execute("insert into rolas ( id_performer, id_album, path, title, track, year, genre) values (?,?,?,?,?,?,?)",data)
                 conn.commit()
         except sqlite3.IntegrityError:
             print('el registro ya existe')
@@ -22,21 +21,74 @@ class Database:
     def insert_in_performers(self,data):
         conn = sqlite3.connect('./../../../../../root/rolas.db')
         db = conn.cursor()
-        db.execute("insert into performers (name) VALUES (?)",data)
+        db.execute("insert into performers (name,id_type) VALUES (?,?)",data)
         conn.commit()
         conn.close()
 
+    def insert_in_albums(self,data):
+        conn = sqlite3.connect('./../../../../../root/rolas.db')
+        db = conn.cursor()
+        db.execute("insert into albums (path, name, year) VALUES (?,?,?)",data)
+        conn.commit()
+        conn.close()
+
+    #Devuelve el id del performer según su nombre
     def check_performers(self, name):
         conn = sqlite3.connect('./../../../../../root/rolas.db')
         db = conn.cursor()
-        db.execute("select id_performer from performers where name = (?)",name)
-        result = db.fetchall()
-        pprint(result)
-        return result
+        rows = db.execute("select id_performer from performers where name = (?)",name)
+        return rows.fetchone()
+        return rows.fetchone()
 
-    def query_result(self,query):
-        pprint(query)
-        #db.execute("insert into rolas (path, title) values (?)",query)
-        #ok = db.fetchall()
-        db.close()
-        return ok
+    #Devuelve el id del album según su nombre
+    def check_albums(self, name):
+        conn = sqlite3.connect('./../../../../../root/rolas.db')
+        db = conn.cursor()
+        rows =db.execute("select id_album from albums where name = (?)",name)
+        return rows.fetchone()
+
+    #Recibe datos de la canción y retorna la canción si todos sus datos son iguales
+    def check_rolas(self,data):
+        conn = sqlite3.connect('./../../../../../root/rolas.db')
+        db = conn.cursor()
+        rows = db.execute("select * from rolas where id_performer = (?) and id_album = (?) and path = (?) and title=(?) and track=(?) and year=(?) and genre=(?)",data)
+        return rows.fetchone()
+
+
+    # Regresa el título de la rola, el performer, el album, el track,
+    # el año y el genre
+    def show_rolas(self) :
+        conn = sqlite3.connect('./../../../../../root/rolas.db')
+        db = conn.cursor()
+        rows = db.execute("select title, performers.name, albums.name, track, rolas.year, genre from rolas INNER JOIN performers INNER JOIN albums")
+        return rows.fetchall() is not None
+
+    def show_rolas_with_performer(self, name):
+         conn = sqlite3.connect('./../../../../../root/rolas.db')
+         db = conn.cursor()
+         rows = db.execute("select title, performers.name, albums.name, track, rolas.year, genre from rolas INNER JOIN performers INNER JOIN albums where performers.name = (?)",name)
+         return rows.fetchall() is not None
+
+    def show_rolas_with_title(self, title):
+         conn = sqlite3.connect('./../../../../../root/rolas.db')
+         db = conn.cursor()
+         rows = db.execute("select title, performers.name, albums.name, track, rolas.year, genre from rolas INNER JOIN performers INNER JOIN albums where rolas.title = (?)",title)
+         return rows.fetchall() is not None
+
+    def show_rolas_with_year(self, year):
+         conn = sqlite3.connect('./../../../../../root/rolas.db')
+         db = conn.cursor()
+         rows = db.execute("select title, performers.name, albums.name, track, rolas.year, genre from rolas INNER JOIN performers INNER JOIN albums where rolas.year = (?)",year)
+         return rows.fetchall() is not None
+
+    def show_rolas_with_genre(self, genre):
+         conn = sqlite3.connect('./../../../../../root/rolas.db')
+         db = conn.cursor()
+         rows = db.execute("select title, performers.name, albums.name, track, rolas.year, genre from rolas INNER JOIN performers INNER JOIN albums where rolas.genre = (?)",genre)
+         return rows.fetchall() is not None
+
+    def show_rolas_with_album(self, name):
+          conn = sqlite3.connect('./../../../../../root/rolas.db')
+          db = conn.cursor()
+          rows = db.execute("select title, performers.name, albums.name, track, rolas.year, genre from rolas INNER JOIN performers INNER JOIN albums where albums.name = (?)",name)
+          return rows.fetchall() is not None
--- reproductor/minero.py
+++ reproductor/minero.py
@@ -13,12 +13,30 @@ class minero:
         title = ''.join(self.__song.get_title())
         track = ''.join(self.__song.get_track())
         year = ''.join(self.__song.get_year())
+        genre = ''.join(self.__song.get_genre())
         album_name = ''.join(self.__song.get_album())
         album_path = ''.join(self.__song.get_album_path())
         performer_name = ''.join(self.__song.get_artist())
         #pprint(performer_name)
         song_to_save = Database()
-        id_performer = song_to_save.check_performers([performer_name])
-        if (len(id_performer) == 0):
-            song_to_save.insert_in_performers([performer_name])
-        #song_to_save.insert_in_rola([(path),(title),(track),(int(year))],[(album_path),(album_name),(int(year))])
+        self.id_performer = song_to_save.check_performers([performer_name])
+        self.id_album = song_to_save.check_albums([album_name])
+        self.rola = ""
+        if( self.id_performer is not None ):
+            if( self.id_album is not None):
+                self.rola=song_to_save.check_rolas([(self.id_performer[0]),(self.id_album[0]),(path),(title),(track),(int(year)),(genre)])
+        pprint(self.id_performer)
+        pprint(self.id_album)
+        #pprint(self.rola)
+
+        if (self.id_performer is None):
+            song_to_save.insert_in_performers([(performer_name),(2)])
+            self.id_performer = song_to_save.check_performers([performer_name])
+
+        if(self.id_album is None):
+            song_to_save.insert_in_albums([(album_path),(album_name),(year)])
+            self.id_album = song_to_save.check_albums([album_name])
+
+        if(self.rola != "" and self.rola is None):
+            song_to_save.insert_in_rola([(self.id_performer[0]),(self.id_album[0]),(path),(title),(track),(int(year)),(genre)])
+            self.rola=self.rola = song_to_save.check_rolas([(self.id_performer[0]),(self.id_album[0]),(path),(title),(track),(int(year)),(genre)])
